b2b29ba213e05bebecdf4f1693ba110a88f07252
Mazen-Al-Samman <mazenalsmman@gmail.com>
2021-07-16 21:22:36 +0300

    Add vendors

--- index.php
+++ index.php
@@ -1,5 +1,69 @@
 <?php 
 
-echo "Hello world";
+echo json_encode([
+    [
+        'id' => 1,
+        'name' => 'City Mall',
+        'country' => 'Jordan',
+        'city' => 'Amman',
+        'img' => 'test.png'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Sameh Mall',
+        'country' => 'Jordan',
+        'city' => 'Amman',
+        'img' => 'test.png'
+    ],
+    [
+        'id' => 3,
+        'name' => 'Mokhtar Mall',
+        'country' => 'Jordan',
+        'city' => 'Amman',
+        'img' => 'test.png'
+    ],
+    [
+        'id' => 4,
+        'name' => 'Leader center',
+        'country' => 'Jordan',
+        'city' => 'Zarqa',
+        'img' => 'test.png'
+    ],
+    [
+        'id' => 5,
+        'name' => 'Mecca Mall',
+        'country' => 'Jordan',
+        'city' => 'Amman',
+        'img' => 'test.png'
+    ],
+    [
+        'id' => 6,
+        'name' => 'Istiklal Mall',
+        'country' => 'Jordan',
+        'city' => 'Irbid',
+        'img' => 'test.png'
+    ],
+    [
+        'id' => 7,
+        'name' => 'Arifa Mall',
+        'country' => 'Jordan',
+        'city' => 'Amman',
+        'img' => 'test.png'
+    ],
+    [
+        'id' => 8,
+        'name' => 'Amman Mall',
+        'country' => 'Jordan',
+        'city' => 'Amman',
+        'img' => 'test.png'
+    ],
+    [
+        'id' => 9,
+        'name' => 'Sameh Mall',
+        'country' => 'Jordan',
+        'city' => 'Zarqa',
+        'img' => 'test.png'
+    ],
+])
 
 ?>
